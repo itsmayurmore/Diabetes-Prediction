03d3ebed456ee0dc999be7ab252853cc14961816
Mayur More <mmayur268@gmail.com>
2022-12-15 20:06:32 +0530

    Changes for Production Server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import sqlite3
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -46,4 +47,4 @@ def result():
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   serve(app,host='0.0.0.0',port='50100')
